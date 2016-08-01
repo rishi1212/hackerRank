1ab6ed1a53178b1c52ef66bde9d5e2b9fc8ecee1
rishi <me.rishiverma@gmail.com>
2016-08-02 02:25:09 +0530

    LisaWorkbook

---
+++ LisaWorkbook.py
@@ -0,0 +1,17 @@
+N, K = map(int, raw_input().split())
+T = map(int, raw_input().split())
+
+cnt = 0
+i = 0
+j = 1
+m = 1
+
+while i < N:
+    if m <= j and j <= min(m + K - 1, T[i]):
+        cnt += 1
+    j += 1
+    m += K
+    if m > T[i]:
+        i += 1
+        m = 1
+print cnt
