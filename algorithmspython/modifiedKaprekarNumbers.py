6bb63f59014f62ffc847d1b4020dfd9ae69ed1f5
rishi <me.rishiverma@gmail.com>
2016-08-17 22:11:01 +0530

    modifiedKaprekarNumbers

---
+++ algorithmspython/modifiedKaprekarNumbers.py
@@ -0,0 +1,20 @@
+def checkIsKaprekar( num ):
+    if(num==1):
+        return True
+    elif((num**2)<15):
+        return False
+    string_num = str(num**2)
+    if num == int(string_num[:len(string_num)//2]) + int(string_num[len(string_num)//2:]):
+        return True
+p=int(input())
+q=int(input())
+number=[]
+for i in range(p,q+1):
+    if(checkIsKaprekar(i)==True):
+        number.append(i)
+
+if(len(number)>0):
+    for i in number:
+        print(i,end=' ')
+else:
+    print("INVALID RANGE")
