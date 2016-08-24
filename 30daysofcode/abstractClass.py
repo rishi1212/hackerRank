5f7bb4c4d55b4033e454d95cfe22c4243dc52703
rishi <me.rishiverma@gmail.com>
2016-08-24 17:12:45 +0530

    abstractClass

---
+++ 30daysofcode/abstractClass.py
@@ -0,0 +1,9 @@
+#Write MyBook class
+class MyBook(Book):
+    def __init__(self,title,author,price):
+        super(Book, self).__init__()
+        self.price=price
+    def display(self):
+        print ("Title: "+ title)
+        print("Author: "+author)
+        print("Price: "+str(price))
