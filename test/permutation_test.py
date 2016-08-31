958f9b062e8cb6319593054082326a38fca38c0f
rishi <me.rishiverma@gmail.com>
2016-08-31 15:43:39 +0530

    permutationstest

---
+++ test/permutation_test.py
@@ -0,0 +1,25 @@
+t = input()
+for _ in range(t):
+    word = list(raw_input().strip())
+    start = -1
+    for i in xrange(0, len(word) - 1):
+        if word[i] < word[i + 1]:
+            start = i
+
+    if start == -1:
+        print "no answer"
+        continue
+
+    end = -1
+    for j in xrange(start + 1, len(word)):
+        if word[start] < word[j]:
+            end = j
+
+    word[start], word[end] = word[end], word[start]
+    a = word[start + 1:]
+    a.sort()
+
+    for j in xrange(start + 1, len(word)):
+        word[j] = a[j - start - 1]
+
+    print "".join(word)
