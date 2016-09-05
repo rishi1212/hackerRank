74a2605313ea9a573b065b6d986b4f43945601ec
rishi <me.rishiverma@gmail.com>
2016-09-05 21:01:01 +0530

    modifiedFibinocci

--- python/collectionMostCommon.py
+++ python/collectionMostCommon.py
@@ -2,4 +2,4 @@ from collections import Counter, OrderedDict
 
 class OrderedCounter(Counter, OrderedDict):
     pass
-[print(*c) for c in OrderedCounter(sorted(input())).most_common(3)]
+#[print(*c) for c in OrderedCounter(sorted(input())).most_common(3)]
