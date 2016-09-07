a4fc818750863a6c77e05cd6373647ddadc524b9
rishi <me.rishiverma@gmail.com>
2016-09-07 17:21:11 +0530

    endstart

--- python/Regex/startend.py
+++ python/Regex/startend.py
@@ -1,3 +1,8 @@
-s, k = input(), input()
-if not [print((i, i + len(k) - 1)) for i in range(len(s) - len(k)) if k in s[i:i + len(k)]]:
-    print((-1, -1))
+# s, k = input(), input()
+# if not [print((i, i + len(k) - 1)) for i in range(len(s) - len(k)) if k in s[i:i + len(k)]]:
+#     print((-1, -1))
+'''
+
+for
+PYTHON 3
+'''
