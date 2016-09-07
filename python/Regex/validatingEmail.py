d1bc1896c8b650129d6e8ee20f5e9189be15568b
rishi <me.rishiverma@gmail.com>
2016-09-07 17:24:35 +0530

    validatingEmail

--- python/Regex/validatingEmail.py
+++ python/Regex/validatingEmail.py
@@ -1,2 +1,6 @@
-import re, sys
-[print(e.strip('\n')) for e in sys.stdin if re.search('<[a-z][a-z0-9_.-]+@[a-z]+\.[a-z]{1,3}>', e, re.I)]
+# import re, sys
+# [print(e.strip('\n')) for e in sys.stdin if re.search('<[a-z][a-z0-9_.-]+@[a-z]+\.[a-z]{1,3}>', e, re.I)]
+'''
+
+python 3
+'''
