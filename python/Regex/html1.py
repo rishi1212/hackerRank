aef0548c3249c62f3b0b574eac8c0dfa32ca8d50
rishi <me.rishiverma@gmail.com>
2016-09-08 20:07:06 +0530

    html1

---
+++ python/Regex/html1.py
@@ -0,0 +1,19 @@
+from HTMLParser import HTMLParser
+import re
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print "Start :", tag
+        for attr in attrs:
+            print "->", attr[0], ">", attr[1]
+    def handle_endtag(self, tag):
+        print "End   :", tag
+    def handle_startendtag(self, tag, attrs):
+        print "Empty :", tag
+        for attr in attrs:
+            print "->", attr[0], ">", attr[1]
+parser = MyHTMLParser()
+texts = str()
+for _ in range(int(raw_input().strip())):
+    texts += raw_input()
+texts = re.sub(r"\<\!--.*?--\>", "", texts)
+parser.feed(texts)
