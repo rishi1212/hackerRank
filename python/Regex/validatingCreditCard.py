013547ce1eb7381141d004aab19c2e462aebd2cc
rishi <me.rishiverma@gmail.com>
2016-09-08 20:13:18 +0530

    validatingCreditCard

---
+++ python/Regex/validatingCreditCard.py
@@ -0,0 +1,18 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+# Check proper contents/starting/count w/o hypen
+normal = r'^([4-6])([0-9]){15}$'
+# Proper contents/separation/count with hypen
+special = r'^([4-6][0-9]{3})-([0-9]{4})-([0-9]{4})-([0-9]{4})$'
+# Match any string w/o 4 non-unique chars
+repeating = r'^(?:(.)(?!-*\1-*\1-*\1))*$'
+
+reg = map(re.compile, [normal, repeating])
+spc = map(re.compile, [special, repeating])
+for _ in xrange(int(raw_input())):
+    l = raw_input().strip()
+    if l.find('-') >=0:
+        tests = map(bool,[x.match(l) for x in spc])
+    else:
+        tests = map(bool,[x.match(l) for x in reg])
+    print 'Valid' if all(tests) else 'Invalid'
