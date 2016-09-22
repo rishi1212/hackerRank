7e61022de1621f2b7f6318731e7ce5dd620baaf8
rishi <me.rishiverma@gmail.com>
2016-09-22 17:42:47 +0530

    minVarStd

--- python/Numpy/minVarStd.py
+++ python/Numpy/minVarStd.py
@@ -1,4 +1,4 @@
 import numpy as np
 n,_=map(int,input().split())
 a=np.array([list(map(int,input().split())) for _ in range(n)])
-print(np.mean(a,axis=1),np.var(a,axis=0),np.std(a),sep='\n')
+print(np.mean(a,axis=1),np.var(a,axis=0),np.std(a))
