597d2332e2e6e96f3430142329bbf66d79b538f9
rishi <me.rishiverma@gmail.com>
2016-10-02 09:53:24 +0530

    Tutorials

---
+++ Tutorials/Cracking the coding interview/arrays:left rotation.py
@@ -0,0 +1,12 @@
+def array_left_rotation(a, n, k):
+    ans=[]
+    ans=a[k:]
+    ans1=(a[:k])
+    ans=ans+ans1
+    return ans
+
+
+n, k = map(int, input().strip().split(' '))
+a = list(map(int, input().strip().split(' ')))
+answer = array_left_rotation(a, n, k);
+print(*answer, sep=' ')
