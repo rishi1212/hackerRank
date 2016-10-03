4aaad51dda468965968cbd3c10e7616588d2f22e
rishi <me.rishiverma@gmail.com>
2016-10-03 21:36:37 +0530

    sorting

--- Tutorials/Cracking the coding interview/arrays:left rotation.py
+++ Tutorials/Cracking the coding interview/arrays:left rotation.py
@@ -9,4 +9,4 @@ def array_left_rotation(a, n, k):
 n, k = map(int, input().strip().split(' '))
 a = list(map(int, input().strip().split(' ')))
 answer = array_left_rotation(a, n, k);
-print(*answer, sep=' ')
+print(answer)
---
+++ algorithmspython/insertionSort-Part-2.py
@@ -0,0 +1,16 @@
+ = int(input())
+a = [int(x) for x in input().split(' ')]
+def printArr(arr):
+    s = '' #NO SPACE
+    for i in range(len(arr)):
+        s = s+ str(arr[i])
+        if (i < len(arr) -1):
+            s = s + ' '
+    print (s)
+for i in range(1,n):
+    for j in range(i):
+        if (a[i-j] < a[i-j-1]):#if(a[i]<a[j])
+            t = a[i-j]			#t=a[i]
+            a[i-j] = a[i-j-1]	#a[i]=a[j]
+            a[i-j-1] = t		#a[j]=t
+    printArr(a)
