b64f18930dd83a345c3aceaac0b809522ce48193
rishi <me.rishiverma@gmail.com>
2016-10-04 14:12:36 +0530

    anagrams

--- Tutorials/Cracking the coding interview/string:making anagram.py
+++ Tutorials/Cracking the coding interview/string:making anagram.py
@@ -4,3 +4,7 @@ def number_needed(a, b):
     ct_b = Counter(b)
     ct_a.subtract(ct_b)
     return sum(abs(i) for i in ct_a.values())
+a = input().strip()
+b = input().strip()
+
+print(number_needed(a, b))
