07ca194d75c34e97864cac823340130845bbf2a3
Rishi <me.rishiverma@gmail.com>
2017-01-18 11:33:45 +0530

    new_2017

--- python/Numpy/dotAndDash.py
+++ python/Numpy/dotAndDash.py
@@ -1,4 +1,6 @@
-import numpy as np
-N = int(raw_input().strip())
-a, b = [np.array([raw_input().strip().split() for i in xrange(N)], int) for j in xrange(2)]
-print np.mat(a) * np.mat(b)
+# import numpy as np
+# from Tools.scripts.treesync import raw_input
+#
+# N = int(raw_input().strip())
+# a, b = [np.array([raw_input().strip().split() for i in xrange(N)], int) for j in xrange(2)]
+# print np.mat(a) * np.mat(b)
