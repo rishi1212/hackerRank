a24775e55c32f09ea0a1627bc837a8bef26014d2
Rishi <me.rishiverma@gmail.com>
2017-01-18 15:59:42 +0530

    sum_min_max

---
+++ algorithms/set.py
@@ -0,0 +1,17 @@
+author="himanshu malhotra"
+aa,bb=0,0
+n,m=map(int,input().split())
+a=map(int,input().split())
+b=map(int,input().split())
+ct=0
+for i in range(max(a),min(b)+1):
+    for j in a:
+        if i%j!=0:
+            break
+    else:
+        for k in b:
+            if k%i!=0:
+                break
+        else:
+            ct+=1
+print ct
