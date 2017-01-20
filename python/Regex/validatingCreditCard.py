133320b714564b54fee47e499b16b1dd15d3b656
Rishi <me.rishiverma@gmail.com>
2017-01-20 16:45:46 +0530

    random

--- python/Regex/validatingCreditCard.py
+++ python/Regex/validatingCreditCard.py
@@ -10,7 +10,7 @@ repeating = r'^(?:(.)(?!-*\1-*\1-*\1))*$'
 reg = map(re.compile, [normal, repeating])
 spc = map(re.compile, [special, repeating])
 for _ in range(int(input())):
-    l = raw_input().strip()
+    l = input().strip()
     if l.find('-') >=0:
         tests = map(bool,[x.match(l) for x in spc])
     else:
