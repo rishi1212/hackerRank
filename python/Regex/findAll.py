02e8462627897b857afbdd1e2aafa81c76904d42
Rishi <me.rishiverma@gmail.com>
2017-01-20 16:41:53 +0530

    random

--- python/Regex/findAll.py
+++ python/Regex/findAll.py
@@ -1,7 +1,7 @@
 import re
 
 
-str1 = raw_input()
+str1 = input()
 consonants='qwrtypsdfghjklzxcvbnm'
 vowels='aeiou'
 m = re.findall(r'(?<=['+consonants+'])(['+vowels+']{2,})(?=['+consonants+'])',str1,flags = re.I)
