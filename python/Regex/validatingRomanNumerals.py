964d259f7a05ae933c759e15b5280cb93e0819ff
Rishi <me.rishiverma@gmail.com>
2017-01-20 16:46:25 +0530

    random

--- python/Regex/validatingRomanNumerals.py
+++ python/Regex/validatingRomanNumerals.py
@@ -1,3 +1,3 @@
 import re
 
-print(bool(re.search(r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$", raw_input())))
+print(bool(re.search(r"^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$",input())))
