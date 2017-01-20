3d4cc4e1f043258d2a05503302a5ac99708135cf
Rishi <me.rishiverma@gmail.com>
2017-01-20 16:44:54 +0530

    random

--- python/Regex/intro.py
+++ python/Regex/intro.py
@@ -3,4 +3,4 @@ import re
 m = int(input())
 for i in range(0,m):
     temp = input()
-    print bool(re.match(r"^[-+]?[0-9]*\.[0-9]+$",temp))
+    print(bool(re.match(r"^[-+]?[0-9]*\.[0-9]+$", temp)))
