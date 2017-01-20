e74cd5d2051a3229367641570d3882bdc8277148
Rishi <me.rishiverma@gmail.com>
2017-01-20 16:47:11 +0530

    random

--- python/randomALl/classes.py
+++ python/randomALl/classes.py
@@ -22,7 +22,7 @@ class complex:
 
 
 A = complex(*map(float, input().strip().split()))
-B = complex(*map(float, raw_input().strip().split()))
+B = complex(*map(float, input().strip().split()))
 
 print A+B
 print A-B
