d9042750ec697af4847a3e935f96cd38b4c4a368
Rishi <me.rishiverma@gmail.com>
2017-01-20 16:57:20 +0530

    code reformat

--- python/randomALl/classes.py
+++ python/randomALl/classes.py
@@ -1,24 +1,33 @@
 from math import pow
+
+
 class complex:
     def __init__(self, real, imag):
         self.real = real
         self.imag = imag
+
     def __add__(self, other):
-        return complex(self.real+other.real, self.imag+other.imag)
+        return complex(self.real + other.real, self.imag + other.imag)
+
     def __sub__(self, other):
-        return complex(self.real-other.real, self.imag-other.imag)
+        return complex(self.real - other.real, self.imag - other.imag)
+
     def __mul__(self, other):
-        return complex(self.real*other.real-self.imag*other.imag, self.real*other.imag+self.imag*other.real)
+        return complex(self.real * other.real - self.imag * other.imag, self.real * other.imag + self.imag * other.real)
+
     def __div__(self, other):
         try:
-            return self.__mul__(complex(other.real, -1*other.imag)).__mul__(complex(1.0/(other.mod().real)**2, 0))
+            return self.__mul__(complex(other.real, -1 * other.imag)).__mul__(complex(1.0 / (other.mod().real) ** 2, 0))
         except ZeroDivisionError as e:
             print(e)
             return None
+
     def mod(self):
-        return complex(pow(self.real**2+self.imag**2, 0.5), 0)
+        return complex(pow(self.real ** 2 + self.imag ** 2, 0.5), 0)
+
     def __str__(self, precision=2):
-        return str(("%."+"%df"%precision)%float(self.real))+('+' if self.imag>=0 else '-')+str(("%."+"%df"%precision)%float(abs(self.imag)))+'i'
+        return str(("%." + "%df" % precision) % float(self.real)) + ('+' if self.imag >= 0 else '-') + str(
+            ("%." + "%df" % precision) % float(abs(self.imag))) + 'i'
 
 
 A = complex(*map(float, input().strip().split()))
