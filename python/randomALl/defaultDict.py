e733da557825f6601d649ecf5f3676e14918b248
Rishi <me.rishiverma@gmail.com>
2017-01-20 16:57:35 +0530

    code reformat

--- python/randomALl/defaultDict.py
+++ python/randomALl/defaultDict.py
@@ -1,14 +1,15 @@
 from collections import defaultdict
+
 d = defaultdict(list)
-list1=[]
+list1 = []
 
-n, m = map(int,input().split())
+n, m = map(int, input().split())
 
-for i in range(0,n):
-    d[input()].append(i+1)
+for i in range(0, n):
+    d[input()].append(i + 1)
 
-for i in range(0,m):
-    list1=list1+[input()]
+for i in range(0, m):
+    list1 = list1 + [input()]
 
 for i in list1:
     if i in d:
