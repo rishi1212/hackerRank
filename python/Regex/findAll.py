ab3ff3c05670efb601e72bd2df4765c6d61d990a
Rishi <me.rishiverma@gmail.com>
2017-01-20 16:42:02 +0530

    random

--- python/Regex/findAll.py
+++ python/Regex/findAll.py
@@ -6,7 +6,7 @@ consonants='qwrtypsdfghjklzxcvbnm'
 vowels='aeiou'
 m = re.findall(r'(?<=['+consonants+'])(['+vowels+']{2,})(?=['+consonants+'])',str1,flags = re.I)
 if not m:
-    print "-1"
+    print("-1")
 else:
     for i in m:
         print i
