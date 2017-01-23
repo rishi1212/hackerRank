67d7a93a8671c83379273c01990ae5cb6f163970
Rishi <me.rishiverma@gmail.com>
2017-01-23 16:33:13 +0530

    code reformat

--- algorithms/set.py
+++ algorithms/set.py
@@ -1,16 +1,16 @@
-aa, bb = 0, 0
-n, m = map(int, input().split())
-a = map(int, input().split())
-b = map(int, input().split())
-ct = 0
-for i in range(max(a), min(b) + 1):
+aa,bb=0,0
+n,m=map(int,raw_input().split())
+a=map(int,raw_input().split())
+b=map(int,raw_input().split())
+ct=0
+for i in xrange(max(a),min(b)+1):
     for j in a:
-        if i % j != 0:
+        if i%j!=0:
             break
     else:
         for k in b:
-            if k % i != 0:
+            if k%i!=0:
                 break
         else:
-            ct += 1
-print(ct)
+            ct+=1
+print ct
