d8e1263b99da36861b4bf05f36b22f61331ac7e4
Rishi <me.rishiverma@gmail.com>
2017-01-23 16:29:20 +0530

    code reformat

--- algorithms/twoChracters.py
+++ algorithms/twoChracters.py
@@ -1,26 +1,27 @@
 import sys
-#sys.stdin=open("in","r")
-n=int(input())
-s=input()
+
+# sys.stdin=open("in","r")
+n = int(input())
+s = input()
 assert s.isalpha()
-ans=0
-for i in range(0,26):
-    for j in range(0,26):
-        if i==j:
+ans = 0
+for i in range(0, 26):
+    for j in range(0, 26):
+        if i == j:
             continue
         p1 = i
         p2 = j
         flag = 1
         l = 0
         for c in s:
-            if ord(c)-ord('a')!=p1 and ord(c)-ord('a')!=p2:
+            if ord(c) - ord('a') != p1 and ord(c) - ord('a') != p2:
                 continue
-            if ord(c)-ord('a') == p1:
+            if ord(c) - ord('a') == p1:
                 l = l + 1
-                p1,p2 = p2,p1
+                p1, p2 = p2, p1
             else:
                 flag = 0
-        if flag == 1 and l>1:
-            ans=max(ans,l)
+        if flag == 1 and l > 1:
+            ans = max(ans, l)
 
 print(ans)
