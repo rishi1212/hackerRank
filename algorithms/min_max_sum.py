b21531b65a269fee64401f3bde6c27f2cada00cd
Rishi <me.rishiverma@gmail.com>
2017-01-23 16:27:50 +0530

    code reformat

--- algorithms/min_max_sum.py
+++ algorithms/min_max_sum.py
@@ -1,11 +1,11 @@
 #!/bin/python
 
-a=list(map(int,input().split(" ")))
+a = list(map(int, input().split(" ")))
 a.sort()
-sum1=0
-sum2=0
-for i in range(0,len(a)-1):
-    sum1+=a[i]
-for j in range(1,len(a)):
-    sum2+=a[j]
-print (sum1,sum2)
+sum1 = 0
+sum2 = 0
+for i in range(0, len(a) - 1):
+    sum1 += a[i]
+for j in range(1, len(a)):
+    sum2 += a[j]
+print(sum1, sum2)
