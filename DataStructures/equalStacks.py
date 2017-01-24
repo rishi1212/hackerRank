8d2bccdd0e31ffc6e24e049a42f23ac63864c729
Rishi <me.rishiverma@gmail.com>
2017-01-24 15:31:11 +0530

    equalStacks

---
+++ DataStructures/equalStacks.py
@@ -0,0 +1,20 @@
+n1, n2, n3 = map(int, raw_input().split())
+H1 = map(int, raw_input().split())[::-1]
+H2 = map(int, raw_input().split())[::-1]
+H3 = map(int, raw_input().split())[::-1]
+
+sum_h1 = sum(H1)
+sum_h2 = sum(H2)
+sum_h3 = sum(H3)
+
+while not (sum_h1 == sum_h2 and sum_h2 == sum_h3):
+    if sum_h1 > sum_h2 or sum_h1 > sum_h3:
+        t = H1.pop()
+        sum_h1 -= t
+    if sum_h2 > sum_h1 or sum_h2 > sum_h3:
+        t = H2.pop()
+        sum_h2 -= t
+    if sum_h3 > sum_h1 or sum_h3 > sum_h2:
+        t = H3.pop()
+        sum_h3 -= t
+print sum_h1
