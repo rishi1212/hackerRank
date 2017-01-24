a29a729ce25a409b86e4976dbd004afbd387a23d
Rishi <me.rishiverma@gmail.com>
2017-01-24 17:13:40 +0530

    apple and orange

--- algorithms/appleAndOrange.py
+++ algorithms/appleAndOrange.py
@@ -2,28 +2,29 @@
 
 import sys
 
+from Tools.scripts.treesync import raw_input
 
-s,t = raw_input().strip().split(' ')
-s,t = [int(s),int(t)]
-a,b = raw_input().strip().split(' ')
-a,b = [int(a),int(b)]
-m,n = raw_input().strip().split(' ')
-m,n = [int(m),int(n)]
-apple = map(int,raw_input().strip().split(' '))
-orange = map(int,raw_input().strip().split(' '))
-appleDistance=[]
-orangeDistance=[]
+s, t = raw_input().strip().split(' ')
+s, t = [int(s), int(t)]
+a, b = raw_input().strip().split(' ')
+a, b = [int(a), int(b)]
+m, n = raw_input().strip().split(' ')
+m, n = [int(m), int(n)]
+apple = map(int, raw_input().strip().split(' '))
+orange = map(int, raw_input().strip().split(' '))
+appleDistance = []
+orangeDistance = []
 for i in apple:
-    appleDistance.append(a+i)
+    appleDistance.append(a + i)
 for j in orange:
-    orangeDistance.append(b+j)
-count1=0
+    orangeDistance.append(b + j)
+count1 = 0
 for i in appleDistance:
-    if(i>=s and i<=t):
-        count1+=1
+    if s <= i <= t:
+        count1 += 1
 print(count1)
-count1=0
+count1 = 0
 for i in orangeDistance:
-    if(i>=s and i<=t):
-        count1+=1
+    if s <= i <= t:
+        count1 += 1
 print(count1)
